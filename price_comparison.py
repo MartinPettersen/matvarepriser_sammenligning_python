865cd11d53f52a8f2e0d158f0785f6c074a087c9
MartinPettersen <searchingtheskyfor@gmail.com>
2024-05-14 08:48:52 +0200

    add calculation for ranking and price increase

--- price_comparison.py
+++ price_comparison.py
@@ -1,12 +1,27 @@
 def price_comparison(price_list):
     rows = []
+    
+    placement = 1
+    latest_price = price_list[0][2]
+    cheapest = price_list[0][2]
+    
     for row in price_list:
+        
+        if row[2] != latest_price:
+            placement += 1
+            latest_price = row[2]
+        
+        one_percent = cheapest / 100
+        more_exspensive = round(((row[2] - cheapest) / one_percent),2)
+        
         store_price = {
             "ean": row[0],
             "store" : row[1],
             "price" : row[2],
             "created_at": row[3],
             "updated_at": row[4],
+            "ranking": placement,
+            "price_increase": more_exspensive,
         }
         rows.append(store_price)
         
